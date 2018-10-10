50c0413419c4429ed4977835d4759e2997f400ac
TurtleFeeder <t.choong@yahoo.com>
2018-10-10 08:35:24 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
-end
+  attr_reader :name, :weight, :height
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+    self.class.all << self
+  end
+end #end Pirate class
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
-end
+  attr_reader :name, :type, :booty
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    self.all.clear
+  end
+
+  def initialize(args)
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
+    self.class.all << self
+  end
+end #end Ship class
